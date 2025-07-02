4da1c54553ae75c9e42439089006217792b5492c
hardik-kanzariya <hardik.digitalskylink@gmail.com>
2025-07-02 16:03:31 +0530

    add live api for getting response of list music

--- lib/resources/constant.dart
+++ lib/resources/constant.dart
@@ -3,3 +3,9 @@ const String song2 = "https://gennii.com/test_audio/2_copy.mpeg";
 const String song3 = "https://gennii.com/test_audio/3_copy.mpeg";
 const String song4 = "https://gennii.com/test_audio/4_copy.mpeg";
 const String song5 = "https://gennii.com/test_audio/5_copy.mpeg";
+
+const String clientId = "d62111b1";
+const String clientSecretId = "56ee384fc386b9e59c364c05c279cd64";
+
+String baseUrl =
+    "https://api.jamendo.com/v3.0/tracks/?client_id=YOUR_CLIENT_ID&format=json&limit=5";
