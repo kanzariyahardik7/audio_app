10016e803d9a7add508cd30c80b28b2b2c721a4b
hardik-kanzariya <hardik.digitalskylink@gmail.com>
2025-08-19 15:38:25 +0530

    dfs

--- lib/resources/constant.dart
+++ lib/resources/constant.dart
@@ -3,9 +3,3 @@ const String song2 = "https://gennii.com/test_audio/2_copy.mpeg";
 const String song3 = "https://gennii.com/test_audio/3_copy.mpeg";
 const String song4 = "https://gennii.com/test_audio/4_copy.mpeg";
 const String song5 = "https://gennii.com/test_audio/5_copy.mpeg";
-
-const String clientId = "d62111b1";
-const String clientSecretId = "56ee384fc386b9e59c364c05c279cd64";
-
-String baseUrl =
-    "https://api.jamendo.com/v3.0/tracks/?client_id=$clientId&format=json&limit=5";
